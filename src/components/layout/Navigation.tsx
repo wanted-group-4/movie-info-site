b7ddb9d66ed5ef5f01c8885e8c05bc8f0ccc8e50
Sohui Suh <greenish0902@gmail.com>
2022-07-08 19:22:35 +0900

    Fix: 랜딩 페이지 라우팅 추가, 네비게이션 바 수정 (#17)

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { AiFillHome, AiOutlineSearch } from 'react-icons/ai';
 
 const Navigation = () => {
+  const formRef = useRef(null);
+  const navigate = useNavigate();
+  const handleSubmit = (event: React.FormEvent): void => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      keyword: { value: string };
+    };
+    const keyword = target.keyword.value;
+    navigate(`search/${keyword}`);
+  };
+
   return (
     <NavWrapper>
-      <NavLink to="">검색</NavLink>
-      <NavLink to="bookmark">즐겨찾기</NavLink>
+      <div>
+        <NavLink to="">
+          <AiFillHome />
+        </NavLink>
+        <NavLink to="bookmark">즐겨찾기</NavLink>
+      </div>
+      {/* 컴포넌트 대체 */}
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <input
+          name="keyword"
+          type="text"
+          placeholder="영화 제목을 검색해보세요"
+        />
+        <AiOutlineSearch className="icon" />
+      </form>
     </NavWrapper>
   );
 };
@@ -14,13 +39,45 @@ const Navigation = () => {
 export default Navigation;
 
 const NavWrapper = styled.nav`
+  position: relative;
   padding: 24px;
+  display: flex;
+  justify-content: space-between;
   a {
     margin: 16px;
-    padding: 8px;
+    padding: 12px;
     text-decoration: none;
     &.active {
       border-bottom: 4px solid ${({ theme }) => theme.color.secondary_01};
+    }
+  }
+  form {
+    margin: 0 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    input {
+      padding: 12px 24px;
+      width: 280px;
+      border-radius: 25px;
+      border: 1px solid #fff;
+      &::placeholder {
+        color: #fff;
+      }
+    }
+  }
+  .icon {
+    position: absolute;
+    right: 48px;
+    font-size: 20px;
+    background-color: transparent;
+  }
+  @media screen and (max-width: 770px) {
+    input {
+      display: none;
+    }
+    .icon {
+      right: 36px;
     }
   }
 `;
