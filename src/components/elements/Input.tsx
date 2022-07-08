e852a79bf34d37574c98a36a7cddedc11859cddc
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-07-09 00:47:49 +0900

    feat: 검색 페이지에 input 컴포넌트button 컴포넌트 이어주기 (#1)
    * design: 검색창 버튼 디자인 설정 (#10)
    
    * [feat] 검색창 인풋 디자인 및 함수 설정하기 (#11)
    
    * feat: 검색 페이지에서 input 컴포넌트에 데이터 내려주기 (#12)
    
    * refactor: type 방식에서 interface 방식으로 변환 및 reset 주석해주기
    
    * feat: SearchRecommendList 컴포넌트 생성하기 (#21)

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, {forwardRef} from 'react';
+import styled from 'styled-components';
 
-const Input = () => {
-  return (
-    <div>Input</div>
-  )
+interface StyledProps {
+  type: string;
+  placeholder?: string;
 }
 
-export default Input
+interface InputProps extends StyledProps {
+  _onKeyPress?: (event: React.KeyboardEvent) => void;
+  _onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  _onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  _onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+}
+
+const Input = forwardRef((
+  {_onChange, _onKeyPress, _onFocus, _onBlur, placeholder, type}: InputProps, ref: React.Ref<HTMLInputElement>) => {
+
+  return (
+    <StyledInput
+      ref={ref}
+      type={type}
+      placeholder={placeholder}
+      onKeyPress={_onKeyPress}
+      onChange={_onChange}
+      onFocus={_onFocus}
+      onBlur={_onBlur}
+    />
+  )
+});
+Input.displayName = 'Input';
+
+Input.defaultProps = {
+  type: 'text',
+  placeholder: '텍스트를 입력해주세요.',
+}
+
+const StyledInput = styled.input`
+  margin: 0 auto;
+  padding: 5px 20px;
+  width: 100%;
+  background: transparent;
+  font-weight: 400;
+  size: 24px;
+  line-height: 34px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${({theme}) => theme.color.white};
+  }
+`;
+
+export default Input;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+import Input from 'src/components/elements/Input';
+import Button from 'src/components/elements/Button';
+import SearchRecommendList from 'src/components/SearchRecommentList';
 
 const Search = () => {
-  return <div>Search</div>;
-};
+  const [searchInput, setSearchInput] = useState('');
+  const [isInputFocus, setIsInputFocus] = useState(false);
+  const [searchData, setSearchData] = useState([{
+    title: '',
+    year: '',
+    id: '',
+    type: '',
+    poster: '',
+    like: false,
+  }]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get('http://localhost:3000/data/movies.json');
+      console.log(data);
+    }
+    fetchData();
+  }, [])
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchInputText = event.target.value;
+    if(searchInputText.length === 0) {
+      return setSearchData([]);
+    }
+  };
+
+  const onCheckEnter = (event: React.KeyboardEvent) => {
+    if(event.key === 'Enter') {
+      searchMovieCallback();
+    }
+  };
+
+  const searchMovieCallback = () => {
+    console.log('검색하고 있습니다~')
+  };
+
+  const focusHandler = () => {
+    setIsInputFocus(true);
+    console.log('포커스되었습니다.')
+    console.log(isInputFocus);
+  };
+
+  const blurHandler = () => {
+    setIsInputFocus(false);
+    console.log('blur 되었습니다');
+    console.log(isInputFocus);
+  };
+
+  return (
+    <SearchContainer>
+      <SearchInputWrapper>
+        <Input
+          _onKeyPress={onCheckEnter}
+          _onChange={onChangeInput}
+          _onFocus={focusHandler}
+          _onBlur={blurHandler}
+          placeholder='영화 제목을 입력해주세요'
+          type='text'
+        />
+        <Button
+          _onClick={searchMovieCallback}
+        />
+      </SearchInputWrapper>
+      {isInputFocus &&
+        <SearchRecommendList />}
+  </SearchContainer>
+)};
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+  border: 2px solid black;
+  width: 80%;
+  border-radius: 20px;
+`;
+
+const SearchInputWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0 auto;
+  width: 100%;
+  border-radius: 20px;
+`;
 
 export default Search;
