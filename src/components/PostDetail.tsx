aa5efe324c9c050cc04fbdb76bcefe4d4928fbbc
mirim <alfla6528@naver.com>
2022-07-09 04:29:53 +0900

    design: 상세페이지 반응형 적용 (#22)

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react'
 import styled, {css} from 'styled-components'
-import { AiOutlineCheckCircle, AiOutlineDown, AiOutlinePlusCircle } from 'react-icons/ai'
+import { AiOutlineArrowLeft, AiOutlineCheckCircle, AiOutlineDown, AiOutlinePlusCircle } from 'react-icons/ai'
 
 interface DetailProps {
-  title?: string;
-  year?: number;
-  runtime?: number;
-  genres?: string[]
-  like?: boolean;
-  background_image?: string;
-  medium_cover_image?: string;
-  src?: string;
-  open?: boolean
+  title: string;
+  year: number;
+  runtime: number;
+  genres: string[]
+  lik?: boolean;
+  description_full?: string;
+  background_image: string;
+  medium_cover_image: string;
 }
 
 const PostDetail = () => {
   const [open, setOpen] = useState(false)
   const [title, year, runtime, genres, like, description_full, background_image, medium_cover_image] =[
-    'Suicide',
+    'Suicide Squad',
     2016,
     123,
     ['Action','Adventure','Fantasy','Sci-Fi'],
@@ -29,13 +28,16 @@ const PostDetail = () => {
 
   return (
     <PostDetailContainer background_image={background_image}>
-      {/* <AiOutlineArrowLeft /> */}
+      <BackButton>
+         <AiOutlineArrowLeft />
+      </BackButton>
+      <MobilePoster url={medium_cover_image}/>
       <Container>
         <Title>{title}</Title>
         <Wrap>
           <StyledBoxWrap>
             <StyledBox>{year}</StyledBox>
-            <StyledBox>{runtime}</StyledBox>
+            <StyledBox>{`${runtime}분`}</StyledBox>
             <StyledBox>{genres[0]}</StyledBox>
           </StyledBoxWrap>
           <LikeButtonWrap>
@@ -49,8 +51,8 @@ const PostDetail = () => {
           <Description >
             <DescriptionText open={open}>{description_full}</DescriptionText>
             <AddButtonWrap>
-            <AddButton onClick={()=>setOpen(!open)}>{open ? '닫기' : '더보기'}</AddButton>
-            <AddIcon open={open}><AiOutlineDown/></AddIcon>
+              <AddButton onClick={()=>setOpen(!open)}>{open ? '닫기' : '더보기'}</AddButton>
+              <AddIcon open={open}><AiOutlineDown/></AddIcon>
             </AddButtonWrap>
           </Description>
         </DescriptionWrap>
@@ -62,44 +64,84 @@ const PostDetail = () => {
 
 export default PostDetail
 
-const PostDetailContainer = styled.div<DetailProps>`
+const BackButton = styled.div`
+  display: none;
+  position: absolute;
+  left: 2%;
+  top: 9%;
+  svg{
+    width:30px;
+    height: 30px;
+  }
+ @media ${({ theme }) => theme.deviceSize.max.mobile} {
+  display: block;
+ }
+`
+const PostDetailContainer = styled.div<{ background_image: string }>`
   display: flex;
   justify-content: space-between;
   width:100vw;
   height:650px;
   padding:5% 9%;
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #212225 100%),
-  url(${props => props.background_image}) no-repeat center;
+  url(${props=> props.background_image}) no-repeat center;
   background-size: cover;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    height: 400px;
+    padding:0;
+    background: none;
+    display: block;
+  }
 `
 
 const Container = styled.div`
   width:55%;
   max-width: 850px;
-  background: none;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    padding:20px;
+    width:100vw;
+  }
+`
+
+const MobilePoster = styled.div<{url:string}>`
+  display: none;
+  width:100vw;
+  height: 450px;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(26, 27, 29, 0.9) 80%, #212225 100%),
+  url(${props => props.url}) no-repeat;
+  background-size: 100%;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    display: block;
+  }
 `
 
 const Title = styled.h1`
   width:430px;
   font-size: 45px;
   font-weight: bold;
-  background: none;
   color:#d9d9d9;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    font-size: 24px;
+    margin-top: 60px;
+  }
 `
 
 const Wrap = styled.div`
   width: 75%;
+  min-width: 340px;
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-  background: none;
   margin: 40px 0;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    margin:15px 0 20px;
+    width: 100%;
+  }
 `
 
 const StyledBoxWrap = styled.div`
   display: flex;
   justify-content: space-between;
-  background: none;
 `
 
 const StyledBox = styled.div`
@@ -111,29 +153,39 @@ const StyledBox = styled.div`
   padding:6px 13px;
   margin-right: 12px;
   color: #d9d9d9;
-  background: none;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    height: 20px;
+    font-size: 13px;
+    padding: 1px 10px;
+    white-space: nowrap;
+  }
 `
 
 const LikeButtonWrap = styled.div`
-  background: none;
   text-align: center;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+
+  }
 `
 
 const LikeButton = styled.div`
-  background: none;
   margin-bottom: 5px;
   svg{
     width:40px;
     height:40px;
-    background: none;
     path{
       color: #d9d9d9;
+    }
+  }
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    svg{
+      width:35px;
+      height:35px;
     }
   }
 `
 
 const LikeButtonText = styled.span`
-  background: none;
   font-size: 14px;
   color: #d9d9d9;
 `
@@ -144,14 +196,13 @@ const DescriptionWrap = styled.div`
 
 const Description = styled.div`
   position:absolute;
-  background: none;
 `
 
-const DescriptionText = styled.p<DetailProps>`
+const DescriptionText = styled.p<{open: boolean}>`
   font-size: 16px;
   line-height: 1.7;
   color: #d9d9d9;
-  ${(props) => !props.open && css`
+  ${props => !props.open && css`
     height:110px;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -159,17 +210,16 @@ const DescriptionText = styled.p<DetailProps>`
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
   `}
-  background: none;
 `
 
 const AddButtonWrap = styled.div`
   margin: 15px 0;
   display: flex;
   align-items: center;
-  background: none;
+
 `
 
-const AddIcon = styled.div<DetailProps>`
+const AddIcon = styled.div<{open:boolean}>`
   width:15px;
   height:15px;
   transform: ${(props) => props.open && 'rotate(180deg)'};
@@ -180,8 +230,8 @@ const AddButton = styled.button`
   font-weight: 600;
   padding: 0;
   margin-right: 7px;
-  background: none;
   color: #d9d9d9;
+  background: none;
   cursor: pointer;
 `
 
@@ -189,5 +239,7 @@ const Poster = styled.img`
   width:330px;
   height:490px;
   margin-right:5%;
-  background: none;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    display: none;
+  }
 `
