0131a73ccdaea95d7d33d9fa5d34054808ecadc5
devmagrfs <devmagrfs@gmail.com>
2022-07-08 16:35:13 +0900

    design: 검색창 버튼 디자인 설정 #10

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import {AiOutlineSearch} from 'react-icons/ai';
 
-const Button = () => {
+const StyledButton = styled.button`
+  margin: 0 auto;
+  padding: 0;
+  width: 10%;
+  background: transparent;
+  border: none;
+`;
+
+interface ButtonProps {
+  _onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  _onClick?: (event: React.MouseEvent) => void;
+}
+
+const Button = ({_onClick}: ButtonProps) => {
   return (
-    <div>Button</div>
+    <StyledButton>
+      <AiOutlineSearch 
+        size={30}
+        onClick={_onClick}
+      />
+    </StyledButton>
   )
 }
 
