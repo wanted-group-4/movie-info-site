a204db9fa9fa97ec495866308d5a326b16594a5a
SungYeolha <tjddufgk@gmail.com>
2022-07-11 20:47:01 +0900

    remove: SearchMovieList 삭제

--- src/components/search/index.ts
+++ src/components/search/index.ts
@@ -1,4 +1,3 @@
 export { default as SearchInput } from './SearchInput';
 export { default as SearchRecentList } from './SearchRecentList';
 export { default as SearchRecommentList } from './SearchRecommentList';
-export { default as SearchMovieList } from './SearchMovieList';
