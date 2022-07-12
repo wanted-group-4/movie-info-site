f6b0b346bb0a701c51932604736c7655c684c112
mirim <alfla6528@naver.com>
2022-07-12 11:35:22 +0900

    feature: movie detail type 추가

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -26,3 +26,15 @@ interface MovieCard {
 }
 
 export { MovieCard };
+  
+export interface MovieDetail {
+  id: number;
+  title: string;
+  year: number;
+  like: boolean;
+  runtime: number;
+  genres: string[];
+  description_full: string;
+  background_image: string;
+  medium_cover_image: string;
+}
