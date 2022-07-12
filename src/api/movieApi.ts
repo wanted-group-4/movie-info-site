c24284f547f3ebf17865fefc1bd8096430806248
JiehoonPark <z1hoon715@gmail.com>
2022-07-13 00:22:51 +0900

    feat: 평점순, 최신순 특정 page 가져오기 쿼리 추가

--- src/api/movieApi.ts
+++ src/api/movieApi.ts
@@ -37,12 +37,16 @@ export const patchMovieFavorite = (id: number, favorite: boolean) => {
     .catch((error) => console.log(error));
 };
 
-export const getMovieByLatest = () => {
-  const response = useHttpRequest('?_sort=date_uploaded&_order=desc&_limit=10');
+export const getMovieByLatestOrder = (page: number) => {
+  const response = useHttpRequest(
+    `?_sort=date_uploaded&_order=desc&_page=${page}&_limit=10`
+  );
   return response;
 };
 
-export const getMovieByRanking = () => {
-  const response = useHttpRequest('?_sort=rating&_order=desc&_limit=10');
+export const getMovieByRankOrder = (page: number) => {
+  const response = useHttpRequest(
+    `?_sort=rating&_order=desc&_page=${page}&_limit=10`
+  );
   return response;
 };
