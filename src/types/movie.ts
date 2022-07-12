64a9e065ba923b58d3ff047436b38f8755b8d470
mirim <alfla6528@naver.com>
2022-07-12 11:45:06 +0900

    feature:  상세페이지 type 추가

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -26,15 +26,3 @@ interface MovieCard {
 }
 
 export { MovieCard };
-
-export interface MovieDetail {
-  id: number;
-  title: string;
-  year: number;
-  like: boolean;
-  runtime: number;
-  genres: string[];
-  description_full: string;
-  background_image: string;
-  medium_cover_image: string;
-}
