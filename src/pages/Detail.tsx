9ac2c87f28882880d77e9f239f2cc3ca6fc27487
mirim <alfla6528@naver.com>
2022-07-13 00:57:01 +0900

    feature: add similar movie list

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router'
+import styled from 'styled-components'
+
+
+import serverApi from 'src/api'
 import { getMovieById } from 'src/api/movieApi'
-import PostDetail from '../components/PostDetail'
+import PostList from 'src/components/PostList'
+import PostDetail from 'src/components/PostDetail'
 
 
 const Detail = () => {
   const { id } = useParams<{ id: string }>()
-  const {data, loading} = getMovieById(id)
+  const { data } = getMovieById(id)
+  const [similarMovie, setSimilarMovie] = useState<any>([])
 
-  return (loading ? <div>Loading...</div>: (data !== null && <PostDetail data={data} />))
+  const getSimilarList = async (genre: string) => {
+    const response = await serverApi.get(`/?genres_like=${genre}`)
+    setSimilarMovie(response)
+  }
+
+  return (
+    <>
+      {data !== null && (
+        <>
+          <PostDetail data={data} getSimilarList={getSimilarList}/>
+          <SimilarMovieWrap>
+            <SimilarMovieTitle>비슷한 영화</SimilarMovieTitle>
+            {similarMovie !== null && <PostList movieList={similarMovie.slice(0,10)}/>}
+          </SimilarMovieWrap>
+        </>
+      )}
+    </>
+  )
 }
 
 export default Detail
+
+const SimilarMovieTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+
+`
+const SimilarMovieWrap = styled.div`
+  width: 90vw;
+  margin: 0 auto;
+  & div {
+    margin-top: 30px;
+  }
+  border-top: 1px solid #d9d9d9;
+  padding-top: 50px;
+`
