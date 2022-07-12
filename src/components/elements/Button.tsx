733c43d48836e088f7e48681571b41d6120e25d6
devmagrfs <devmagrfs@gmail.com>
2022-07-12 21:13:37 +0900

    fix: 병합 오류 수정

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AiOutlineSearch } from 'react-icons/ai';
 
 interface ButtonProps {
-  _onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  _onClick?: (event: React.MouseEvent) => void;
+  children: React.ReactNode;
+  onClick?: (event: React.MouseEvent) => void;
 }
 
 const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
