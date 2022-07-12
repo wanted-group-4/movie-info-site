f6ef0dab1a3a820e57e8f4820c936b6779fd52cf
Mirim Lee <mrlee3233@gmail.com>
2022-07-12 21:00:34 +0900

    design : 상세페이지 반응형적용 (#35)
    * design: 반응형 적용 & navigate 추가
    
    * refactor: json-server 파일이동 server 실행 경로변경
    
    * refactor: json 파일 경로 root로 이동

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
-import styled, {css} from 'styled-components'
+import styled, { css } from 'styled-components'
+import { useNavigate } from 'react-router'
 import { AiOutlineArrowLeft, AiOutlineCheckCircle, AiOutlineDown, AiOutlinePlusCircle } from 'react-icons/ai'
+
 import { MovieDetail } from 'src/types/Movie'
 import { patchMovieFavorite } from 'src/api/movieApi'
 
+
 const PostDetail = ({data}: any) => {
   const [open, setOpen] = useState(false)
+  const navigate = useNavigate()
   const {
     id,title, year, runtime, genres, like, background_image, medium_cover_image, description_full }: MovieDetail = data
 
   return (
     <PostDetailContainer background_image={background_image}>
-      <BackButton>
+      <BackButton onClick={()=>navigate(-1)}>
          <AiOutlineArrowLeft />
       </BackButton>
+      <MobilePoster url={medium_cover_image}/>
       <Container>
         <Title>{title}</Title>
         <Wrap>
@@ -46,15 +51,21 @@ const PostDetail = ({data}: any) => {
 
 export default PostDetail
 
-const PostDetailContainer = styled.div<{background_image: string}>`
+const PostDetailContainer = styled.div<{ background_image: string }>`
   display: flex;
   justify-content: space-between;
   width:100vw;
   height:650px;
   padding:5% 9%;
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #212225 100%),
-  url(${props => props.background_image}) no-repeat center;
+  url(${props=> props.background_image}) no-repeat center;
   background-size: cover;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    height: 400px;
+    padding:0;
+    background: none;
+    display: block;
+  }
 `
 
 const BackButton = styled.div`
@@ -63,42 +74,63 @@ const BackButton = styled.div`
   left: 2%;
   top: 9%;
   svg{
-    width:30px;
-    height: 30px;
+    width:25px;
+    height: 25px;
   }
  @media ${({ theme }) => theme.deviceSize.max.mobile} {
   display: block;
  }
 `
 
-
 const Container = styled.div`
   width:55%;
   max-width: 850px;
-  background: none;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    padding:20px;
+    width:100vw;
+  }
+`
+
+const MobilePoster = styled.div<{url:string}>`
+  display: none;
+  width:100vw;
+  height: 450px;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(26, 27, 29, 0.9) 80%, #212225 100%),
+  url(${props => props.url}) no-repeat;
+  background-size: 100%;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    display: block;
+  }
 `
 
 const Title = styled.h1`
   width:430px;
   font-size: 45px;
   font-weight: bold;
-  background: none;
   color:#d9d9d9;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    width: 80vw;
+    font-size: 24px;
+    margin-top: 60px;
+  }
 `
 
 const Wrap = styled.div`
   width: 75%;
+  min-width: 270px;
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-  background: none;
   margin: 40px 0;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    margin:15px 0 20px;
+    width: 100%;
+  }
 `
 
 const StyledBoxWrap = styled.div`
   display: flex;
   justify-content: space-between;
-  background: none;
 `
 
 const StyledBox = styled.div`
@@ -110,29 +142,39 @@ const StyledBox = styled.div`
   padding:6px 13px;
   margin-right: 12px;
   color: #d9d9d9;
-  background: none;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    height: 20px;
+    font-size: 13px;
+    padding: 1px 10px;
+    white-space: nowrap;
+  }
 `
 
 const LikeButtonWrap = styled.div`
-  background: none;
   text-align: center;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    
+  }
 `
 
 const LikeButton = styled.div`
-  background: none;
   margin-bottom: 5px;
   svg{
     width:40px;
     height:40px;
-    background: none;
     path{
       color: #d9d9d9;
+    }
+  }
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    svg{
+      width:35px;
+      height:35px;
     }
   }
 `
 
 const LikeButtonText = styled.span`
-  background: none;
   font-size: 14px;
   color: #d9d9d9;
 `
@@ -143,31 +185,29 @@ const DescriptionWrap = styled.div`
 
 const Description = styled.div`
   position:absolute;
-  background: none;
 `
 
 const DescriptionText = styled.p<{open: boolean}>`
   font-size: 16px;
   line-height: 1.7;
   color: #d9d9d9;
-  ${(props) => !props.open && css`
+  ${props => !props.open && css`
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
   `}
-  background: none;
 `
 
 const AddButtonWrap = styled.div`
   margin: 15px 0;
   display: flex;
   align-items: center;
-  background: none;
+
 `
 
-const AddIcon = styled.div<{open: boolean}>`
+const AddIcon = styled.div<{open:boolean}>`
   width:15px;
   height:15px;
   transform: ${(props) => props.open && 'rotate(180deg)'};
@@ -178,8 +218,8 @@ const AddButton = styled.button`
   font-weight: 600;
   padding: 0;
   margin-right: 7px;
-  background: none;
   color: #d9d9d9;
+  background: none;
   cursor: pointer;
 `
 
@@ -187,5 +227,7 @@ const Poster = styled.img`
   width:330px;
   height:490px;
   margin-right:5%;
-  background: none;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    display: none;
+  }
 `
