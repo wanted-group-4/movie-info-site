4121c169f81001e1c268b03e0e5cdea8b129a693
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-07-13 18:33:21 +0900

    feat: input value에 따라 리스트 변환 (#21)
    * input 값에 따라 검색어 리스트 변환
    * 로컬 스토리지에 최근 검색어 저장 정상화

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -13,13 +13,20 @@ interface SearchInputProps {
 
 const SearchInput: React.FC<SearchInputProps> = ({ handleSearchMovie }) => {
   const { data } = getMovies() as { data: IMovie[] };
+  const loadedRecentKeyword = localStorage.getItem('recentKeyword')
+    ? JSON.parse(localStorage.getItem('recentKeyword')!)
+    : [];
 
+  const [recentKeyword, setRecentKeyword] = useState(loadedRecentKeyword);
   const [searchInput, setSearchInput] = useState<string>('');
   const [filterMovie, setFilterMovie] = useState<IMovie[] | []>([]);
   const [isInputFocus, setIsInputFocus] = useState<boolean>(false);
-  const [recentKeyword, setRecentKeyword] = useState(
-    JSON.parse(localStorage.getItem('recentkeyword') || '[]')
-  );
+
+  const handleInnterText = (innerText: string) => {
+    console.log(innerText);
+    setSearchInput(innerText.trim());
+    handleSearch();
+  };
 
   const sendQuery = (query: string) => {
     const recommendMovie = filterSearchMovie(query);
@@ -79,7 +86,10 @@ const SearchInput: React.FC<SearchInputProps> = ({ handleSearchMovie }) => {
       ? [...recentKeyword.slice(0, -1)]
       : recentKeyword;
 
-    localStorage.setItem('recentKeyword', JSON.stringify(newRecentKeyword));
+    window.localStorage.setItem(
+      'recentKeyword',
+      JSON.stringify(newRecentKeyword)
+    );
     setRecentKeyword(newRecentKeyword);
   }, [recentKeyword]);
 
@@ -92,7 +102,7 @@ const SearchInput: React.FC<SearchInputProps> = ({ handleSearchMovie }) => {
               onKeyDown={handleKeyDown}
               onChange={handleDebounce}
               onFocus={handleCheckIsFocus}
-              onBlur={handleCheckIsFocus}
+              // onBlur={handleCheckIsFocus}
               placeholder="영화 제목을 입력해주세요"
               type="text"
             />
@@ -104,6 +114,8 @@ const SearchInput: React.FC<SearchInputProps> = ({ handleSearchMovie }) => {
             <SearchList
               recentKeyword={recentKeyword}
               filterMovie={filterMovie}
+              handleInnterText={handleInnterText}
+              searchInput={searchInput}
             />
           )}
         </SearchInputWrap>
