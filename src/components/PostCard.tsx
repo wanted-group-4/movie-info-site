230bc1b224d34709e23170e76fa9fbe850e917a2
Sohui Suh <greenish0902@gmail.com>
2022-07-13 12:28:15 +0900

    fix: 카드 컴포넌트, 스와이퍼 수정 (#48)
    * fix: api 타입 수정
    
    * chore: noimage.png 추가
    
    * feat: PostCard 컴포넌트 라우팅
    
    * fix: props 수정
    
    * refactor: CardProps 인터페이스 분리
    
    * design: 반응형 css, 가로 스크롤 추가
    
    * design: PostCard border 수정
    
    * feat: state, margin을 이용한 슬라이더 구현
    
    * feat: 770px 이하 슬라이더 구현

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 import styled from 'styled-components';
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 
-interface propTypes {
+interface PostCardProps {
   data: {
-    id?: number;
     title: string;
     summary: string;
     medium_cover_image: string;
     like: boolean;
+    id: number;
   };
-  onClick?: (event: React.MouseEvent) => void;
+  handleSelect: (index: number) => void;
+  cardIdx: number;
+  selectedIdx: number;
 }
 
-const PostCard = (props: propTypes) => {
-  const { data, onClick } = props;
+const PostCard = (props: PostCardProps) => {
+  const [loading, setLoading] = useState(true);
+  const { data, selectedIdx, cardIdx } = props;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`detail/${data.id}`);
+  };
+  const handleLoad = () => {
+    setLoading(() => false);
+  };
+  const handleError = (
+    event: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    event.currentTarget.src = '/images/noimage.png';
+  };
+  const setSelect = () => {
+    cardIdx === selectedIdx
+      ? props.handleSelect(-1)
+      : props.handleSelect(cardIdx);
+  };
+  const handleSelect = () => {
+    setSelect();
+    handleNavigate();
+  };
 
   return (
-    <PostCardContainer onClick={onClick}>
-      <img src={data.medium_cover_image} alt={data.title} />
+    <PostCardContainer
+      onClick={handleSelect}
+      style={{ display: loading ? 'none' : 'block' }}
+      className={cardIdx === selectedIdx ? 'selected' : ''}
+    >
+      <div className="imgBox">
+        <img
+          src={data.medium_cover_image}
+          alt={data.title}
+          onLoad={handleLoad}
+          onError={handleError}
+        />
+      </div>
       <div className="text">
+        <div className="icons">
+          {data.like ? <MdFavorite /> : <MdFavoriteBorder />}
+        </div>
         <div className="title">{data.title}</div>
         {data.summary && (
           <div className="desc">
@@ -38,14 +79,24 @@ const PostCardContainer = styled.li`
   cursor: pointer;
   position: relative;
   transition: all 300ms ease;
-  &:hover {
+  user-select: none;
+  &.selected {
     z-index: 9999;
-    transform: scale(1.2);
+    transform: scale(1.3);
+    .text {
+      z-index: 999;
+      opacity: 0.9;
+      overflow: scroll;
+    }
   }
-  img {
-    width: 100%;
-    height: 100%;
-    border-radius: 4px;
+  .imgBox {
+    width: 200px;
+    height: 300px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 4px;
+    }
   }
   .text {
     position: absolute;
@@ -55,15 +106,28 @@ const PostCardContainer = styled.li`
     height: 100%;
     z-index: 999;
     line-height: 1.5;
-    overflow: hidden;
     text-overflow: ellipsis;
     word-wrap: break-word;
     transition: all 600ms ease;
+    border: 16px solid transparent;
     background-color: #212225;
-    border: 16px solid #212225;
     opacity: 0;
+    overflow: hidden;
+    .icons {
+      position: fixed;
+      top: 4px;
+      right: 4px;
+      * {
+        margin: 0 2px;
+        transition: all 300ms ease;
+        font-size: 12px;
+        &:hover {
+          transform: scale(1.1);
+          color: ${({ theme }) => theme.color.secondary_01};
+        }
+      }
+    }
     .title {
-      border: 12px solid #212225;
       display: -webkit-box;
       -webkit-line-clamp: 2; /* 라인수 */
       -webkit-box-orient: vertical;
@@ -71,10 +135,13 @@ const PostCardContainer = styled.li`
       text-align: center;
       font-size: 20px;
       font-weight: 800;
+      border-top: 12px solid #212225;
+      border-bottom: 12px solid #212225;
     }
     .desc {
       font-size: 12px;
       word-wrap: break-word;
+      word-break: break-all;
       line-height: 1.7;
       .summary {
         font-size: 14px;
@@ -90,12 +157,30 @@ const PostCardContainer = styled.li`
       z-index: 999;
       opacity: 0.9;
     }
+    .details {
+      color: white;
+      background-color: black;
+    }
   }
   @media screen and (max-width: 770px) {
-    img,
+    .imgBox,
     .text {
-      width: 160px;
-      height: 100%;
+      width: 120px;
+      height: 180px;
+    }
+    .text {
+      border: 12px solid transparent;
+      .title {
+        font-size: 12px;
+      }
+      .desc {
+        font-size: 10px;
+        .summary {
+          margin-right: 4px;
+          padding: 1px 2px;
+          font-size: 8px;
+        }
+      }
     }
   }
 `;
