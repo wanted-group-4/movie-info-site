aee3880ea76313fc231e75ced42329dc1f509828
Mirim Lee <mrlee3233@gmail.com>
2022-07-13 11:17:22 +0900

    feature: 상세페이지 영화리스트 & 슬라이더 추가  (#46)
    * feature: add similar movie list
    
    * feature: get width& height hook
    
    * feature: random array
    
    * feature: slider 구현
    
    * feature: add list of similar movies
    
    * feature: add like function
    
    * refactor: move file

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,14 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router'
-import { getMovieById } from 'src/api/movieApi'
-import PostDetail from '../components/PostDetail'
+import styled from 'styled-components'
 
+import serverApi from 'src/api'
+import { getMovieById } from 'src/api/movieApi'
+import PostDetail from 'src/components/PostDetail'
+import randomIndex from 'src/utils/randomIndex'
+import Slider from 'src/components/slider/Slider'
 
 const Detail = () => {
   const { id } = useParams<{ id: string }>()
-  const {data, loading} = getMovieById(id)
+  const { data } = getMovieById(id)
+  const [similarMovie, setSimilarMovie] = useState<any>([])
 
-  return (loading ? <div>Loading...</div>: (data !== null && <PostDetail data={data} />))
+  const getSimilarList = async (genre: string) => {
+    const response: any= await serverApi.get(`/?genres_like=${genre}`)
+    const array = randomIndex(response.length,30).map(x=> response[x])
+    setSimilarMovie(array)
+  }
+
+
+  return (
+    <>
+      {data !== null && (
+        <Backgound background_image={data.background_image}>
+          <PostDetail data={data} getSimilarList={getSimilarList}/>
+          <SimilarMovieWrap>
+            <SimilarMovieTitle>비슷한 영화</SimilarMovieTitle>
+            {similarMovie !== null && <Slider movieList={similarMovie}/>}
+          </SimilarMovieWrap>
+        </Backgound>
+      )}
+    </>
+  )
 }
 
 export default Detail
+
+const Backgound = styled.div<{ background_image: string }>`
+  position: relative;
+  width: 100vw;
+  height: 600px;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #212225 100%),
+  url(${props=> props.background_image}) no-repeat center;
+  background-size: cover;
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    background: none;
+  }
+`
+
+const SimilarMovieTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+
+`
+const SimilarMovieWrap = styled.div`
+  position: relative;
+  width: 96%;
+  margin-left: 15px;
+  padding-left: 20px;
+  box-sizing: border-box;
+  border-top: 1px solid #d9d9d9;
+  padding-top: 50px;
+  & div {
+    margin-top: 20px;
+  }
+  @media ${({ theme }) => theme.deviceSize.max.mobile} {
+    margin: 0 auto;
+    padding-left: 0
+  }
+`
