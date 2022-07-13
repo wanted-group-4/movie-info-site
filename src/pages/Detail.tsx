56fbb9863b027ec8c8e6cc6feefbe84b2bbda2b0
Sungyeolha <tjddufgk@gmail.com>
2022-07-13 12:48:59 +0900

    fix: 타입 수정 (#41)
    * fix: useHttpRequest 타입수정
    
    * fix: any타입 타입 구체화 작업

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,59 +1,58 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
+import styled from 'styled-components';
 
-import serverApi from 'src/api'
-import { getMovieById } from 'src/api/movieApi'
-import PostDetail from 'src/components/PostDetail'
-import randomIndex from 'src/utils/randomIndex'
-import Slider from 'src/components/slider/Slider'
+import serverApi from 'src/api';
+import { getMovieById } from 'src/api/movieApi';
+import PostDetail from 'src/components/PostDetail';
+import randomIndex from 'src/utils/randomIndex';
+import Slider from 'src/components/slider/Slider';
+import { IMovie } from 'src/types/Movie';
 
 const Detail = () => {
-  const { id } = useParams<{ id: string }>()
-  const { data } = getMovieById(id)
-  const [similarMovie, setSimilarMovie] = useState<any>([])
+  const { id } = useParams() as unknown as { id: number };
+  const { data } = getMovieById(id) as { data: IMovie };
+  const [similarMovie, setSimilarMovie] = useState<any>([]);
 
   const getSimilarList = async (genre: string) => {
-    const response: any= await serverApi.get(`/?genres_like=${genre}`)
-    const array = randomIndex(response.length,30).map(x=> response[x])
-    setSimilarMovie(array)
-  }
-
+    const response: any = await serverApi.get(`/?genres_like=${genre}`);
+    const array = randomIndex(response.length, 30).map((x) => response[x]);
+    setSimilarMovie(array);
+  };
 
   return (
     <>
       {data !== null && (
         <Backgound background_image={data.background_image}>
-          <PostDetail data={data} getSimilarList={getSimilarList}/>
+          <PostDetail movie={data} getSimilarList={getSimilarList} />
           <SimilarMovieWrap>
             <SimilarMovieTitle>비슷한 영화</SimilarMovieTitle>
-            {similarMovie !== null && <Slider movieList={similarMovie}/>}
+            {similarMovie !== null && <Slider movieList={similarMovie} />}
           </SimilarMovieWrap>
         </Backgound>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
 
 const Backgound = styled.div<{ background_image: string }>`
   position: relative;
   width: 100vw;
   height: 600px;
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #212225 100%),
-  url(${props=> props.background_image}) no-repeat center;
+    url(${(props) => props.background_image}) no-repeat center;
   background-size: cover;
   @media ${({ theme }) => theme.deviceSize.max.mobile} {
     background: none;
   }
-`
+`;
 
 const SimilarMovieTitle = styled.h1`
   font-size: 30px;
   font-weight: 700;
-
-`
+`;
 const SimilarMovieWrap = styled.div`
   position: relative;
   width: 96%;
@@ -67,6 +66,6 @@ const SimilarMovieWrap = styled.div`
   }
   @media ${({ theme }) => theme.deviceSize.max.mobile} {
     margin: 0 auto;
-    padding-left: 0
+    padding-left: 0;
   }
-`
+`;
